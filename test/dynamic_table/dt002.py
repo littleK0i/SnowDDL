b244b6a84fe07aa8858001d00c51a6395f87ed31
Vitaly Markov <wild.desu@gmail.com>
2023-07-10 11:22:03 +0100

    Swap execution order of DYNAMIC_TABLE and EVENT_TABLE, simplify dynamic table test to avoid Snowflake bug in dynamic table dependencies

--- test/dynamic_table/dt002.py
+++ test/dynamic_table/dt002.py
@@ -28,10 +28,10 @@ def test_step3(helper):
     dynamic_table_show_1 = helper.show_dynamic_table("db1", "sc1", "dt002_dt1")
     dynamic_table_show_2 = helper.show_dynamic_table("db1", "sc1", "dt002_dt2")
 
-    assert dynamic_table_show_1['target_lag'] == "5 minutes"
-    assert "count(*) AS item_cnt" in dynamic_table_show_1['text']
-    assert "dt002_dt2" in dynamic_table_show_1['text']
+    assert dynamic_table_show_1['target_lag'] == "DOWNSTREAM"
+    assert "count(*) AS name_cnt" in dynamic_table_show_1['text']
+    assert "dt002_tb2" in dynamic_table_show_1['text']
 
-    assert dynamic_table_show_2['target_lag'] == "DOWNSTREAM"
-    assert "count(*) AS name_cnt" in dynamic_table_show_2['text']
-    assert "dt002_tb2" in dynamic_table_show_2['text']
+    assert dynamic_table_show_2['target_lag'] == "5 minutes"
+    assert "count(*) AS item_cnt" in dynamic_table_show_2['text']
+    assert "dt002_dt1" in dynamic_table_show_2['text']
