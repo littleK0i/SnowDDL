f3ffba5e224f8e96b674d316df3f6cff43c9077c
Vitaly Markov <wild.desu@gmail.com>
2023-10-12 19:13:15 +0100

    Fix missing .items() call

--- snowddl/_config/sample02_01/__custom/02_test.py
+++ snowddl/_config/sample02_01/__custom/02_test.py
@@ -5,7 +5,7 @@ def handler(config: SnowDDLConfig):
     # Add view combining all custom tables
     parts = []
 
-    for full_name, bp in config.get_blueprints_by_type_and_pattern(TableBlueprint, "test_db.test_schema.custom_table_*"):
+    for full_name, bp in config.get_blueprints_by_type_and_pattern(TableBlueprint, "test_db.test_schema.custom_table_*").items():
         parts.append(f"SELECT id, name FROM {full_name}")
 
     bp = ViewBlueprint(
