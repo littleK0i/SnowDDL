657c01e5ab22478e7370e0928b2343b11134bad2
Vitaly Markov <wild.desu@gmail.com>
2024-12-03 20:01:13 +0000

    Add test objects for permission models, databases and schemas

--- test/_config/step2/__custom/cu002.py
+++ test/_config/step2/__custom/cu002.py
@@ -1,6 +1,6 @@
-from snowddl import SnowDDLConfig, TableBlueprint
+from snowddl import SnowDDLConfig, TableBlueprint, IdentPattern
 
 
 def handler(config: SnowDDLConfig):
-    for full_name, bp in config.get_blueprints_by_type_and_pattern(TableBlueprint, "db1.sc1.cu002*").items():
+    for full_name, bp in config.get_blueprints_by_type_and_pattern(TableBlueprint, IdentPattern("db1.sc1.cu002*")).items():
         config.remove_blueprint(bp)
